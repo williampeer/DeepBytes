7b70b81daf5316b7a98a42b1f1b4c4148ea39607
William Berg <williapb@gmail.com>
2016-01-25 19:44:02 +0100

    Reverting to older, working commit.

--- DNMM.py
+++ DNMM.py
@@ -1,7 +1,7 @@
 import time
 from HPC import *
 from SimpleNeocorticalNetwork import *
-from data import *
+from data_capital import *
 
 def hpc_learn_patterns_wrapper(hpc, patterns, training_iterations):
     print "Commencing learning of", len(patterns), "I/O patterns."
@@ -17,21 +17,31 @@ def hpc_learn_patterns_wrapper(hpc, patterns, training_iterations):
         for i in xrange(training_iterations):
             # one iteration of learning using Hebbian learning
             hpc.learn()
+            hpc.print_activation_values_sum()
         time_after = time.time()
         train_time = time_after - time_before
-        print "Training time: ", "{:10.4f}".format(train_time), "seconds. Trained for", training_iterations, "iterations."
+        print "Training time: ", "{:10.4f}".format(train_time), "seconds. Trained for", training_iterations, "iteration(s)."
     time_stop_overall = time.time()
-    print "Learned", len(patterns), "patterns in ", "{:10.4f}".format(time_stop_overall-time_start_overall), "seconds."
+    print "Learned", len(patterns), "pattern-associations in ", "{:10.4f}".format(time_stop_overall-time_start_overall), "seconds."
 
-def hpc_chaotic_recall_wrapper(hpc, display_images_of_intermediate_output):
+def hpc_chaotic_recall_wrapper(hpc, display_images_of_intermediate_output, recall_iterations):
     time_before = time.time()
-    hpc.setup_input(np.random.random((1, hpc.input_values.get_value().shape[1])).astype(np.float32))
-    hpc.recall_until_stability_criteria(should_display_image=display_images_of_intermediate_output)
-    time_after = time.time()
-    prop_time_until_stable = time_after - time_before
-    # print "output:", hpc.output_values.get_value()
+    cur_iters = 0
+    while cur_iters < recall_iterations:
+        new_random_input = np.ones_like(hpc.input_values.get_value(), dtype=np.float32)
+        np.random.seed(np.sqrt(time.time()).astype(np.int64))
+        for rand_in_index in xrange(new_random_input.shape[1]):
+            if np.random.random() < 0.5:
+                new_random_input[0][rand_in_index] = -1
+        hpc.setup_input(new_random_input)
+        cur_iters += hpc.recall_until_stability_criteria(should_display_image=display_images_of_intermediate_output,
+                                                         max_iterations=recall_iterations-cur_iters)
+        time_after = time.time()
+        prop_time_until_stable = time_after - time_before
 
-    print "Propagation time until stability:", "{:0.3f}".format(prop_time_until_stable), "seconds."
+        print "Propagation time until stability:", "{:0.3f}".format(prop_time_until_stable), "seconds."
+        print "t =", cur_iters
+        time_before = time.time()
 
 # ==================== TESTING CODE: ======================
 # Hippocampal module
@@ -43,26 +53,12 @@ io_dim = 49
 hpc = HPC([io_dim, 240, 1600, 480, io_dim],
           0.67, 0.25, 0.04,  # connection rates: (in_ec, ec_dg, dg_ca3)
           0.10, 0.01, 0.04,  # firing rates: (ec, dg, ca3)
-          0.7, 1, 0.1, 0.5,  # gamma, epsilon, nu, turnover rate
+          0.7, 1.0, 0.1, 0.5,  # gamma, epsilon, nu, turnover rate
           0.10, 0.95, 0.8, 2.0)  # k_m, k_r, a_i, alpha, alpha is 2, 4, and 5 in different experiments in Hattori (2014)
-# sample IO:
-I = np.asarray([[1, -1, 1, -1, 1, -1, 1] * 7], dtype=np.float32)
-O = np.asarray([[-1, 1, -1, 1, -1, 1, -1] * 7], dtype=np.float32)
-
-rand_I = np.random.random((1, io_dim)).astype(np.float32) - 0.5 * np.ones((1, io_dim), dtype=np.float32)
-rand_O = np.random.random((1, io_dim)).astype(np.float32) - 0.5 * np.ones((1, io_dim), dtype=np.float32)
-for index in xrange(rand_I.shape[1]):
-    if rand_I[0][index] < 0:
-        rand_I[0][index] = -1
-    else:
-        rand_I[0][index] = 1
-    if rand_O[0][index] < 0:
-        rand_O[0][index] = -1
-    else:
-        rand_O[0][index] = 1
 
 patterns = []
-for pattern in data_letters:
+num_to_learn = 1
+for pattern in data_letters_capital[:num_to_learn]:
     io = [[]]
     for row in pattern:
         for el in row:
@@ -72,16 +68,17 @@ for pattern in data_letters:
 # patterns.reverse()
 hpc.print_info()
 hpc_learn_patterns_wrapper(hpc, patterns=patterns, training_iterations=1)
-hpc.print_info()
+# hpc.print_info()
 
 # hpc_chaotic_recall_wrapper(hpc, display_images_of_intermediate_output=False)
 
 print "Recalling all learned patterns:"
-for i in xrange(len(patterns)):
+for i in xrange(len(patterns[:num_to_learn])):
     print "Pattern #", i
     hpc.setup_input(patterns[i][0])
     hpc.recall()
     hpc.show_image_from(hpc.output_values.get_value())
+    hpc.print_activation_values_sum()
 
 hpc.print_info()
 
