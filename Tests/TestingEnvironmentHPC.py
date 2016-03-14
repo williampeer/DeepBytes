377d16135b4a03917385692dd9a92f53d27a443e
williampeer <williapb@gmail.com>
2016-03-14 15:16:29 +0100

    Started writing a straightforward weights update pass in order to optimize it outside of the model.

--- Tests/TestingEnvironmentHPC.py
+++ Tests/TestingEnvironmentHPC.py
@@ -1,8 +1,10 @@
 from HPC import *
 from Tools import save_images_from
+import Tools
 import cPickle
 
 io_dim = 49
+theano.config.floatX = 'float32'
 
 hpc = HPC([io_dim, 240, 1600, 480, io_dim],
           0.67, 0.25, 0.04,  # connection rates: (in_ec, ec_dg, dg_ca3)
@@ -10,14 +12,31 @@ hpc = HPC([io_dim, 240, 1600, 480, io_dim],
           0.7, 1, 0.1, 0.5,  # gamma, epsilon, nu, turnover rate
           0.10, 0.95, 0.8, 2.0)  # k_m, k_r, a_i, alpha
 
-I = np.asarray([[1, -1, 1, -1, 1, -1, 1] * 7], dtype=np.float32)
-save_images_from([I])
+# I = np.asarray([[1, -1, 1, -1, 1, -1, 1] * 7], dtype=np.float32)
+# save_images_from([I])
+
+l1 = theano.shared(value=Tools.binomial_f(1, 49, 0.5).astype('float32'), borrow=True)
+l2 = theano.shared(value=np.zeros_like(l1, dtype=np.float32).astype('float32'), borrow=True)
+n_dim = l1.get_value().shape[1]
+print("n_dim:", n_dim)  # ->49
+Ws = theano.shared(value=Tools.binomial_f(n_dim, n_dim, 0.5).astype('float32'), borrow=True)
+l2_firing_rate = 0.1023
+
+l1_in = T.fmatrix()
+Ws_in = T.fmatrix()
+next_l2_vals = l1_in.dot(Ws_in)
+fire_to_l2 = theano.function([l1_in, Ws_in], updates=[(l2, next_l2_vals)])
+
+# fire_to_l2(l1.get_value(), Ws.get_value())
+print("l2 vals before kWTA:", l2.get_value())
+# l2 = hpc.kWTA(l2.get_value(), l2_firing_rate)
+# print("l2:", l2.get_value())
 
 
 # test_vals = np.random.random((1, 100)).astype(np.float32)
 # test_vals = 0.312987 * np.ones((1, 100), dtype=np.float32)
 # t0 = time.time()
-# result = kWTA(test_vals, 0.20)
+# result = hpc.kWTA(test_vals, 0.20)
 # t1 = time.time()
 # print result
 # print "np.sum(result[0]):", np.sum(result[0])
