77853a32035c5cb92c32dd8120206aa7d202969c
williampeer <williapb@gmail.com>
2016-03-30 14:50:14 +0200

    Debugging.

--- Experiments_4_x.py
+++ Experiments_4_x.py
@@ -45,7 +45,7 @@ def training_and_recall_hpc_helper(hpc, training_set_size, train_set_num, origin
     print "Recalling patterns for", chaotic_recall_iters, "time-steps by chaotic recall..."
     t2 = time.time()
     [patterns_extracted_for_current_set, random_in] = \
-        hpc_chaotic_recall_wrapper(hpc, display_images_of_stable_output=False, recall_iterations=chaotic_recall_iters)
+        hpc_chaotic_recall_wrapper(hpc, display_images_of_stable_output=True, recall_iterations=chaotic_recall_iters)
     for pat in patterns_extracted_for_current_set:
         if not set_contains_pattern(hippocampal_chaotic_recall_patterns, pat):
             hippocampal_chaotic_recall_patterns.append(pat)  # append unique pattern
