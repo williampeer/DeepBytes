be422baf1104a7e93e41741c6c8b9e3be2cecc8c
williampeer <williapb@gmail.com>
2016-03-30 19:13:18 +0200

    Ported show_image_ca3 from the Weights.. branch

--- Tools.py
+++ Tools.py
@@ -60,6 +60,21 @@ def create_image_helper(in_values):
     return im
 
 
+def show_image_ca3(in_values):
+    pattern = np.asarray(in_values, dtype=np.float32)
+    width = 24
+    height = 20
+    pixel_scaling_factor = 2 ** 3
+    im = Image.new('1', (width*pixel_scaling_factor, height*pixel_scaling_factor))
+    for element in range(pattern.shape[1]):
+        for i in range(pixel_scaling_factor):
+            for j in range(pixel_scaling_factor):
+                im.putpixel(((element % width)*pixel_scaling_factor + j,
+                             np.floor(element/width).astype(np.int8) * pixel_scaling_factor + i),
+                            pattern[0][element] * 255)
+    im.show()
+
+
 shared_random_generator = RandomStreams()
 
 x_r = T.iscalar()
