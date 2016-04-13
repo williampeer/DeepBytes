5197ef4caa3c7c89eceb4a0fcb63105c65ba5652
williampeer <williapb@gmail.com>
2016-04-13 19:01:01 +0200

    Updated the parser to include set_size and #sets in the return vector

--- saved_data/Parser.py
+++ saved_data/Parser.py
@@ -1,4 +1,5 @@
 
+# returns: [ experiment: [set_size, #sets, DGW, 5x tuples: [iters_to_convergence, distinct_patterns_recalled]]]
 def get_data_from_log_file(filename):
     log_file = file(filename, 'r')
 
@@ -10,13 +11,15 @@ def get_data_from_log_file(filename):
     for experiment_ctr in range(len(lines)/11):
         experiment_data = []
 
-        # sep = lines[11*experiment_ctr].split('x')
-        # set_size = int(sep[0][-1])
-        # set_parts = int(sep[0][0])
+        sep = lines[11*experiment_ctr].split('x')
+        set_size = int(sep[0][len(sep[0])-1])
+        training_sets = int(sep[1][0])
 
         words = lines[experiment_ctr * 11].split()
         dg_weighting = words[-1][:len(words[-1])-1]
 
+        experiment_data.append(set_size)
+        experiment_data.append(training_sets)
         experiment_data.append(float(dg_weighting))
         for i in range(5):
             convergence_line_words = lines[11 * experiment_ctr + 1 + 2 * i].split()
