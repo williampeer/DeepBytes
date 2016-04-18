a6344fc241f60eab387314f985e8d4b435838bb7
williampeer <williapb@gmail.com>
2016-04-18 15:54:54 +0200

    Updated the parser and plotlib. Refactored.

--- saved_data/Parser.py
+++ saved_data/Parser.py
@@ -1,4 +1,5 @@
 
+
 # returns: [ experiment: [set_size, #sets, DGW, 5x tuples: [iters_to_convergence, distinct_patterns_recalled]]]
 def get_data_from_log_file(filename):
     log_file = file(filename, 'r')
@@ -32,6 +33,47 @@ def get_data_from_log_file(filename):
         all_data.append(experiment_data)
 
     return all_data
-#
-# filename = 'log.txt'  # log_file_path
-# print get_data_from_log_file(filename)
+
+
+def get_convergence_and_distinct_patterns_from_log_v1(parsed_data):
+    experiment_data_convergence = []
+    experiment_data_distinct_patterns = []
+    for i in range(5):
+        experiment_data_convergence.append([])
+        experiment_data_distinct_patterns.append([])
+
+    num_of_experiments = len(parsed_data)
+    for i in range(num_of_experiments):
+        set_size = parsed_data[i][0]
+        num_of_sets = parsed_data[i][1]
+        dg_weighting = parsed_data[i][2]
+
+        for data_tuple in parsed_data[i][3:]:
+            iterations_to_convergence_or_max = data_tuple[0]
+            distinct_patterns_recalled = data_tuple[1]
+
+            experiment_data_convergence[set_size-2].append(iterations_to_convergence_or_max)
+            experiment_data_distinct_patterns[set_size-2].append(distinct_patterns_recalled)
+
+    return [experiment_data_convergence, experiment_data_distinct_patterns]
+
+
+def get_convergence_info_and_distinct_patterns_from_data(set_size, convergence_data, distinct_patterns_data):
+    converged_ctr = 0
+    convergence_sum = 0
+    for converge_el in convergence_data:
+        convergence_sum += converge_el
+        if converge_el < 50:
+            converged_ctr += 1
+
+    distinct_patterns_sum = 0
+    for el in distinct_patterns_data:
+        distinct_patterns_sum += el
+
+    avg_convergence_iters = convergence_sum / float(len(convergence_data))
+    convergence_ratio = converged_ctr / float(len(convergence_data))
+
+    avg_dist_patterns = distinct_patterns_sum / float(len(distinct_patterns_data))
+    recall_ratio = avg_dist_patterns / float(set_size)
+
+    return [[avg_convergence_iters, convergence_ratio], [avg_dist_patterns, recall_ratio]]
--- saved_data/PlotLib.py
+++ saved_data/PlotLib.py
@@ -8,28 +8,5 @@ import Parser
 parsed_data = Parser.get_data_from_log_file('log.txt')
 # print parsed_data[0][3:]
 
-experiment_data_convergence = []
-experiment_data_distinct_patterns = []
-for i in range(5):
-    experiment_data_convergence.append([])
-    experiment_data_distinct_patterns.append([])
-
-num_of_experiments = len(parsed_data)
-for i in range(num_of_experiments):
-    set_size = parsed_data[i][0]
-    num_of_sets = parsed_data[i][1]
-    dg_weighting = parsed_data[i][2]
-
-    for data_tuple in parsed_data[i][3:]:
-        iterations_to_convergence_or_max = data_tuple[0]
-        distinct_patterns_recalled = data_tuple[1]
-
-        experiment_data_convergence[set_size-2].append(iterations_to_convergence_or_max)
-        experiment_data_distinct_patterns[set_size-2].append(distinct_patterns_recalled)
-
-print "data for a number of sets =:", len(experiment_data_distinct_patterns[0])
-print experiment_data_distinct_patterns[0]
-print experiment_data_distinct_patterns[0][:25]
-print experiment_data_distinct_patterns[0][25:50]
-print experiment_data_distinct_patterns[0][50:75]
-print experiment_data_distinct_patterns[0][75:100]
+convergence_data, distinct_patterns_data = Parser.get_convergence_and_distinct_patterns_from_log_v1(parsed_data)
+# convergence_data[0]: convergence data for the first set size.
