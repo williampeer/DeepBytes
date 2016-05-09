01ae2e7551d73c6f36c3379ca6c97581faaee9be
williampeer <williapb@gmail.com>
2016-05-09 16:28:50 +0200

    Added file for trying to fix dubplicate log lines

---
+++ saved_data/fix-dgw-log.py
@@ -0,0 +1,17 @@
+
+
+log_file = file('dgw-exps-log.txt', 'r')
+contents = log_file.read()
+log_file.close()
+
+lines = contents.split('\n')
+
+corrected_contents = ""
+
+for line_ctr in range(len(lines)):
+    if line_ctr % 14 != 0:
+        corrected_contents += lines[line_ctr]
+
+log_f = file('dgw-exps-corrected.txt', 'wb')
+log_f.write(corrected_contents)
+log_f.close()
