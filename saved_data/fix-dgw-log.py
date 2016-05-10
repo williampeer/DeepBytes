acf6b7e79eb7193a0b5d3c0b5d100d719d2a6b2f
williampeer <williapb@gmail.com>
2016-05-10 14:16:21 +0200

    UPdated fix-file.

--- saved_data/fix-dgw-log.py
+++ saved_data/fix-dgw-log.py
@@ -1,16 +1,17 @@
 
 
-log_file = file('dgw-exps-corrected.txt', 'r')
+log_file = file('dgw-exps-log.txt', 'r')
 contents = log_file.read()
 log_file.close()
 
-contents = contents.replace("4.11", "4.1")
+contents = contents.replace("4_x_", "4.")
 lines = contents.split('\n')
 
 corrected_contents = ""
 
 for line_ctr in range(len(lines)):
-    corrected_contents += lines[line_ctr] + '\n'
+    if line_ctr % 14 != 0:
+        corrected_contents += lines[line_ctr] + '\n'
 
 log_f = file('dgw-exps-corrected.txt', 'wb')
 log_f.write(corrected_contents)
